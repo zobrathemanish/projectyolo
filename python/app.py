436e4286e28d17ee73b5c8770be7b9a36236bf24
Manish <manish@bottle.com.np>
2019-02-07 18:17:26 +0545

    no image.show

--- python/app.py
+++ python/app.py
@@ -36,8 +36,8 @@ def upload():
         image=Image.open(fullpath)
         image.save(fullpath)
         result = darknet.detect_image(fullpath)
-        image = Image.open('./image.jpg')
-        image.show()
+        #image = Image.open('./image.jpg')
+        #image.show()
         return jsonify({"result": result})
      
 @application.route('/', methods=['GET', 'POST'])
